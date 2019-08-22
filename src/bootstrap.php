ecab6ed16cf6557109d544901b6c1d2537416790
jevgeny-jonas <jevgeny.jonas@gmail.com>
2019-08-22 23:17:25 +0300

    Make bootstrap work in different setups.
    (Both when this package is installed as dependency or deployed as main
    project.)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
 
-require_once __DIR__ .'/../vendor/autoload.php';
+if (is_dir(__DIR__ .'/../vendor'))
+{
+	require_once __DIR__ .'/../vendor/autoload.php';
+}
+//else: this package is installed as dependency (it is itself in vendor dir), main project's autoload.php does everything
 
 require_once \eugenejonas\php_stuff\config\Config::$ROOT_DIR .'/src/include.php';
 
